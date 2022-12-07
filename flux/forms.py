bb727f1f55898f8ce04b7f2898b9a068869f49c4
Olivier <looolive@gmail.com>
2022-12-07 12:25:33 +0100

    flux v2 + changed the form to create a ticket and a review in one operation

--- flux/forms.py
+++ flux/forms.py
@@ -6,14 +6,17 @@ from django.contrib.auth.models import User
 class TicketForm(forms.ModelForm):
     class Meta:
         model = Ticket
-        fields = ["title", "description", "image", "user"]
-        widgets = {'description': forms.Textarea, 'user': forms.HiddenInput()}
+        # fields = ["title", "description", "image", "user"]
+        # widgets = {'description': forms.Textarea, 'user': forms.HiddenInput()}
+        fields = ["title", "description", "image"]
+        widgets = {'description': forms.Textarea}
 
 
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
-        fields = ["headline", "body", "rating", "user", "ticket"]
-        # widgets = {'body': forms.Textarea, 'user': forms.HiddenInput()}
-        widgets = {'body': forms.Textarea, 'user': forms.HiddenInput(), 'ticket': forms.HiddenInput()}
+        # fields = ["headline", "body", "rating", "user", "ticket"]
+        # widgets = {'body': forms.Textarea, 'user': forms.HiddenInput(), 'ticket': forms.HiddenInput()}
+        fields = ["headline", "body", "rating"]
+        widgets = {'body': forms.Textarea}
 
--- flux/views.py
+++ flux/views.py
@@ -10,9 +10,6 @@ from .models import Ticket, Review
 from account.models import UserFollows
 
 
-DUMMY_TICKET_TITLE = 'dummy_ticket_dkfrjz$54KGlsf@sdf!6412tdp_zztop'
-
-
 @login_required
 def edit_ticket(request, pk=None):
     """
@@ -27,9 +24,11 @@ def edit_ticket(request, pk=None):
         ticket = None
 
     if request.method == "POST":
-        form = TicketForm(request.POST, request.FILES, instance=ticket, initial={'user': request.user})
+        form = TicketForm(request.POST, request.FILES, instance=ticket)
         if form.is_valid():
-            updated_ticket = form.save()
+            updated_ticket = form.save(commit=False)
+            updated_ticket.user = request.user
+            updated_ticket.save()
             if ticket is None:
                 messages.success(request, f"Le ticket {updated_ticket} a été créé.")
             else:
@@ -37,7 +36,7 @@ def edit_ticket(request, pk=None):
 
             return redirect("edit_ticket", updated_ticket.pk)
     else:
-        form = TicketForm(instance=ticket, initial={'user': request.user})
+        form = TicketForm(instance=ticket)
 
     return render(request, 'flux/create_ticket.html', {'section': 'posts', "method": request.method, "form": form})
 
@@ -61,7 +60,12 @@ def create_review_from_ticket(request, pk=None):
         # Creates the review
         form_review = ReviewForm(request.POST)
         if form_review.is_valid():
-            created_review = form_review.save()
+            # created_review = form_review.save()
+            created_review = form_review.save(commit=False)
+            created_review.user = request.user
+            created_review.ticket = get_object_or_404(Ticket, pk=pk)  # new
+            created_review.ticket.save()  # new
+            created_review.save()  # new
             messages.success(request, f"La critique {created_review} a été créée")
             return redirect("index")
 
@@ -72,42 +76,24 @@ def create_review_from_ticket(request, pk=None):
 
 @login_required
 def create_review(request):
-    """
-    Creates a ticket and a review with two forms and one validation.
-    First create a dummy ticket, and update it with post data
-    In the review form, the foreign key to ticket is an hidden input. It has to be there to validate the form.
-    """
-    if not Ticket.objects.filter(title=DUMMY_TICKET_TITLE):
-        dummy_ticket = Ticket.objects.create(title=DUMMY_TICKET_TITLE, description='dummy_desc',
-                                             user=request.user)
-    else:
-        dummy_ticket = Ticket.objects.get(title=DUMMY_TICKET_TITLE)
-
-    if request.method == 'GET':
-        form_ticket = TicketForm(initial={'user': request.user})
-        form_review = ReviewForm(initial={'user': request.user, 'ticket': dummy_ticket})
-
-    else:  # Post
-        form_ticket = TicketForm(request.POST, request.FILES, instance=dummy_ticket)
-        if form_ticket.is_valid():
-            ticket = form_ticket.save()
-
-            form_review = ReviewForm(request.POST)
-            if form_review.is_valid():
-                review = form_review.save(commit=False)
-                review.ticket = ticket
-                review.save()
-
-                messages.success(request, f"La critique {review} a été créée")
-                return redirect("index")
+    form_ticket = TicketForm()
+    form_review = ReviewForm()
+    if request.method == 'POST':
+        form_ticket = TicketForm(request.POST, request.FILES)
+        form_review = ReviewForm(request.POST)
+        if all([form_ticket.is_valid(), form_review.is_valid()]):
+            ticket = form_ticket.save(commit=False)
+            ticket.user = request.user
+            ticket.save()
+            review = form_review.save(commit=False)
+            review.user = request.user
+            review.ticket = ticket
+            review.save()
+            messages.success(request, f"La critique {review} a été créée")
+            return redirect("index")
 
     return render(request, 'flux/create_review.html',
                   {"section": 'posts', "form_ticket": form_ticket, "form_review": form_review})
-
-
-def delete_dummy_ticket(request):
-    if Ticket.objects.filter(title=DUMMY_TICKET_TITLE, user=request.user):
-        Ticket.objects.filter(title=DUMMY_TICKET_TITLE, user=request.user).delete()
 
 
 def get_users_viewable_tickets(user):
@@ -116,7 +102,6 @@ def get_users_viewable_tickets(user):
     """
     followed_users = UserFollows.objects.filter(user_id=user.id)
     users_viewable_tickets = [user.followed_user_id for user in followed_users]
-    # users_viewable_tickets.append(user.id)
     tickets = Ticket.objects.filter(user_id__in=users_viewable_tickets)
     return tickets
 
@@ -135,16 +120,18 @@ def get_users_viewable_reviews(user):
 
 @login_required
 def feed(request):
-    delete_dummy_ticket(request)
     reviews = get_users_viewable_reviews(request.user)
     # returns queryset of reviews
     reviews = reviews.annotate(content_type=Value('REVIEW', CharField()))
 
     tickets = get_users_viewable_tickets(request.user)
-    # returns queryset of tickets
     tickets = tickets.annotate(content_type=Value('TICKET', CharField()))
 
-    # return render(request, 'flux/index.html', context={'tickets': tickets, 'reviews': reviews, 'section': 'flux'})
+    # Get ticket review
+    for ticket in tickets:
+        if ticket.review_set.all():  # has a review
+            review = ticket.review_set.filter(ticket=ticket)
+            ticket.rev = review.first()
 
     # # combine and sort the two types of posts
     posts = sorted(
@@ -155,3 +142,6 @@ def feed(request):
     return render(request, 'flux/index.html', context={'posts': posts, 'section': 'flux'})
 
 
+@login_required
+def my_posts(request):
+    pass
